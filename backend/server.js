8bffeb17f48de5191b5b774c7ca877f37446ca5b
amit77t <amitchaurasia774@gmail.com>
2026-01-22 23:14:34 +0530

    add frontend url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:5173', 'https://authentication-system-kohl.vercel.app/' ];
+const allowedOrigins = ['http://localhost:5173', 'https://authentication-system-kohl.vercel.app' ];
 
 app.use(express.json());
 app.use(cookieParser());
